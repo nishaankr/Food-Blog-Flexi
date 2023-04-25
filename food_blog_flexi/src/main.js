1a43d47110cfbdd225f992f17b1ed28783734e58
akki912 <aakash2001holikatti@gmail.com>
2023-04-25 16:30:07 +0530

    Created BlogPost.vue

--- food_blog_flexi/src/main.js
+++ food_blog_flexi/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Vue2Editor from "vue2-editor"; 
+
+Vue.use(Vue2Editor);
 
 Vue.config.productionTip = false
 
