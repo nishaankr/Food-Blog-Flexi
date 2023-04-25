ae1379eb8a17d9412ae637db649252062a794300
nishaankr <nishaankr@gmail.com>
2023-04-26 00:02:04 +0530

    Footer Added

--- food_blog_flexi/src/components/Navigation.vue
+++ food_blog_flexi/src/components/Navigation.vue
@@ -3,7 +3,7 @@
     <nav class="container">
       <div class="branding">
         <router-link class="header" :to="{ name: 'Home' }"
-          >FireBlogs</router-link
+          >FoodBlog</router-link
         >
       </div>
       <div class="nav-links">
