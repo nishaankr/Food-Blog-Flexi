8bbef1ce357435cb2a75ee82be54cb19880f7142
nishaankr <nishaankr@gmail.com>
2023-04-25 16:38:26 +0530

    Dependencies, fire connection and

--- food_blog_flexi/src/App.vue
+++ food_blog_flexi/src/App.vue
@@ -4,11 +4,12 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Raleway:ital@1&display=swap");
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- food_blog_flexi/src/main.js
+++ food_blog_flexi/src/main.js
@@ -1,13 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import Vue2Editor from "vue2-editor";
 
-Vue.config.productionTip = false
+Vue.use(Vue2Editor);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  render: function (h) {
+    return h(App);
+  },
+}).$mount("#app");
